921c84808f2edaa9d8feae57525c116fd01df55a
Amirouche <amirouche.boubekki@dolead.com>
2018-05-10 22:22:28 +0200

    add found.v510.subspace

--- src/found/v510/__init__.py
+++ src/found/v510/__init__.py
@@ -1,10 +1,14 @@
 from found.v510.base import open
 from found.v510.tuple import pack
+from found.v510.tuple import pack_with_versionstamp
 from found.v510.tuple import unpack
-
+from found.v510.tuple import range
+from found.v510.subspace import Subspace
 
 __all__ = [
     open,
     pack,
+    pack_with_versionstamp,
     unpack,
+    range,
 ]
