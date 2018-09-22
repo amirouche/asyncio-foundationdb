d54801a2d21ed18d64a90977a4fe955a3cab4f5f
Amirouche <amirouche@hypermove.net>
2018-09-22 18:44:42 +0200

    bump v0.3.1 because pypi is a pain

--- found/__init__.py
+++ found/__init__.py
@@ -32,7 +32,7 @@ from fdb.tuple import unpack  # noqa
 from fdb.subspace_impl import Subspace  # noqa
 
 
-__VERSION__ = (0, 3, 0)
+__VERSION__ = (0, 3, 1)
 VERSION = "v" + ".".join([str(x) for x in __VERSION__])
 
 
