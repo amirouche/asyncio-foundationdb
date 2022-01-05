8c351d6d37ca88be530e0234f2138f93a23f22a3
Amirouche <amirouche@hyper.dev>
2022-01-05 13:14:44 +0100

    wip

--- ci.sh
+++ ci.sh
@@ -1,6 +1,6 @@
 PYTHON_MAJOR_MINOR=$1
 export PATH=$HOME/.local/bin:$PATH
-./python-compile.sh $PYTHON_MAJOR_MINOR
+./python-compile.sh $PYTHON_MAJOR_MINOR > /dev/null
 rm -rf .venv
 echo "exit()" | sh venv python$PYTHON_MAJOR_MINOR
 ./venv pip install poetry
