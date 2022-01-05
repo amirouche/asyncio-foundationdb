fd520ae82c4fc8a41a6275f90414dea87608b9a1
Amirouche <amirouche@hyper.dev>
2022-01-05 13:15:05 +0100

    wip

--- ci.sh
+++ ci.sh
@@ -6,7 +6,7 @@ echo "exit()" | sh venv python$PYTHON_MAJOR_MINOR
 ./venv pip install poetry
 ./venv make init
 ./venv make check
-# Publish if there is tag on the current commit
+# Publish if there is a tag on the current commit
 set +x
 git tag -l --points-at $(git show -q --format=%H) | grep v && ./venv poetry config http-basic.pypi __token__ $(cat ~/.pypi-token) || true
 set -x
